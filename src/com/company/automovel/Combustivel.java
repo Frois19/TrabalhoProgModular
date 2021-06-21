f33f67a046a62eaa3a7ff77a44731852ed8a1b06
Frois <joao.victor.frois@gmail.com>
2021-06-21 18:43:55 -0300

    Coreção de atibutos da classe enum Combustivel

--- src/com/company/automovel/Combustivel.java
+++ src/com/company/automovel/Combustivel.java
@@ -3,8 +3,8 @@ package com.company.automovel;
 public enum Combustivel {
     Diesel(4, 4.34), Gasolina(10, 5.79), Alcool(7, 4.49);
 
-    private double consumo;
-    private double precoMedio;
+    public double consumo;
+    public double precoMedio;
 
     Combustivel(double consumo, double precoMedio){
         this.consumo = consumo;
